ba2912eeb628bf2121aa0ec625bf5b5e34642644
Kevin Doolittle <kldpd22@gmail.com>
2022-04-16 13:51:49 -0700

    html, css, and js changes and testing

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,9 +5,11 @@ var clearButton = document.getElementById('clear');
 var cityName = document.getElementById('city');
 let city = document.getElementById('city').value.trim();
 
+var currentWeather = {};
+
 console.log(city);
 function getApi(cityName) {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey//'https://api.openweathermap.org/data/2.5/onecall?lat=33.42&lon=111.94&units=imperial&appid=ece933bf2ec6b5665201a9e858129794';
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + apiKey//'https://api.openweathermap.org/data/2.5/onecall?lat=33.42&lon=111.94&units=imperial&appid=ece933bf2ec6b5665201a9e858129794';
     //var testCity = "api.openweathermap.org/data/2.5/forecast?q=" + "Phoenix" + "&appid=" + apiKey
 
     fetch(requestUrl)
@@ -16,36 +18,51 @@ function getApi(cityName) {
         })
         .then(function (data) {
             console.log(data);
+            currentWeather.name = data.city.name;
+            currentWeather.temp = data.temp;
+            currentWeather.wind = data.list.wind.speed;
+            currentWeather.humidity = data.humidity;
         })
 
-    /* var currentWeather = {
-         temp: data.temp,
-         cityName: data.name,
-         wind: data.wind.speed,
-         humidity: data.humidity,
-         uvIndex: data.uvi,
-     }
- 
-     var currentWeatherCont = {
- 
-     }
- 
-     function addCurrentWeather() {
-         var ul = document.getElementById("city");
-         var li = document.createElement("li");
-         li.appendChild(document.textContent);
-         ul.appendChild(li);
-     }
- */
+    //  var currentWeather = {  //index these data values at the index in the data for the date so current day would be index of 0 
+    //      temp: data.temp,
+    //      city: data.name,
+    //      wind: data.wind.speed,
+    //      humidity: data.humidity,
+    //      uvIndex: data.uvi,
+    //  }
+
+    function addCurrentWeather() { // append data to html element 
+        var ul = document.getElementById("current-city");
+        var li = document.createElement("li");
+        li.appendChild(currentWeather);
+        ul.appendChild(li);
+
+        var fiveDayWeather = { // index these data values for the 5 day forecast at index 1-5 
+            temp: data.temp,
+            wind: data.wind.speed,
+            humidity: data.humidity,
+        }
+
+        function addFiveDay() { // append data to html element
+            var ul2 = document.getElementsByClassName('five-day');
+            var li2 = document.createElement('li');
+            li2.appendChild(fiveDay);
+            ul2.appendChild(li2);
+        }
+    }
+
 };
 
 function revealFiveDay() {
+    var current = document.getElementById('current-city');
     var dayOne = document.getElementById('day-1')
     var dayTwo = document.getElementById('day-2')
     var dayThree = document.getElementById('day-3')
     var dayFour = document.getElementById('day-4')
     var dayFive = document.getElementById('day-5')
 
+    current.style.display = "block";
     dayOne.style.display = "block";
     dayTwo.style.display = "block";
     dayThree.style.display = "block";
@@ -56,16 +73,11 @@ function revealFiveDay() {
 
 function getCity() {
     // event.preventDefault();
-    let city = document.getElementById('city').value.trim();
+    var city = document.getElementById('city').value.trim();
     console.log(getApi(city));
     revealFiveDay();
 }
 
-
-// getCity();
-// console.log('city');
-// $('search').on('click', getCity)
-// $('city').on('click', getApi())
 getApi();
 
 // searchButton.addEventListener('click', getCity());
